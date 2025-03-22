c8ca4f2389f0ac9e5e4060f21d9612a705b7ccc6
Yra <yurchik.yuri.ru@yandex.ru>
2025-03-22 12:53:53 +0500

    method str.trim()

--- scripts.js
+++ scripts.js
@@ -15,180 +15,75 @@
 
 // // Код возьмите из предыдущего домашнего задания
 
-// 'use strict';
+'use strict';
 
-// let numberOfFilms;
+let numberOfFilms;
 
-// function start() {
-//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-//     }
-// }
-
-// start();
-
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
-
-
-
-// function rememberMyFilms() {
-//     for (let i = 0; i < 2; i++) {
-//         const a = prompt('Один из последних просмотренных фильмов?', ''),
-//             b = prompt('На сколько оцените его?', '');
-
-//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//             personalMovieDB.movies[a] = b;
-//             console.log('done');
-//         } else {
-//             console.log('error');
-//             i--;
-//         }
-//     }
-// }
-
-// // rememberMyFilms();
-
-
-
-// function detectPersonalLevel() {
-//     if (personalMovieDB.count < 10) {
-//         console.log("Просмотрено довольно мало фильмов");
-//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-//         console.log("Вы классический зритель");
-//     } else if (personalMovieDB.count >= 30) {
-//         console.log("Вы киноман");
-//     } else {
-//         console.log("Произошла ошибка");
-//     }
-// }
-
-// // detectPersonalLevel();
-
-// function showMyDB(hidden) {
-//     if (!hidden) {
-//         console.log(personalMovieDB);
-//     }
-// }
-
-// showMyDB(personalMovieDB.privat);
-
-// function writeYourGenres() {
-//     for (let i = 1; i <= 3; i++) {
-//         personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-//     }
-// }
-
-// writeYourGenres();
-
-// -------------------- 1 задача -------------------
-
-function calculateVolumeAndArea(edge) {
-    const volume = edge * edge * edge;
-    const area = 6 * (edge * edge);
-
-    if (typeof (edge) !== 'number' || edge <= 0 || !Number.isInteger(edge)) {
-        console.log("При вычислении произошла ошибка");
-    } else {
-        console.log(`Объем куба: ${volume}, площадь всей поверхности: ${area}`);
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
     }
 }
 
-calculateVolumeAndArea('5');
+start();
 
-// -------------------- 2 задача -------------------
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-function getCoopeNumber(place) {
-    coope = place / 4;
-    if (typeof (place) !== 'number' || place < 0 || !Number.isInteger(place)) {
-        return "Ошибка. Проверьте правильность введенного номера места";
-    }
 
-    if (place === 0 || place > 36) {
-        return "Таких мест в вагоне не существует";
-    }
 
-    return Math.ceil(place / 4);
-}
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+            b = prompt('На сколько оцените его?', '');
 
-console.log(getCoopeNumber(12));
-
-// -------------------- 3 задача -------------------
-
-function getTimeFromMinutes(time) {
-    const hours = Math.floor(time / 60);
-    const minutes = time % 60;
-
-    let strHours = ''
-
-    switch (hours) {
-        case 1:
-            strHours = 'час';
-            break;
-
-        case 2:
-        case 3:
-        case 4:
-            strHours = 'часа';
-            break;
-
-        default:
-            strHours = 'часов';
-            break;
-    }
-
-    return `Это ${hours} ${strHours} и ${minutes} минут`
-}
-
-console.log(getTimeFromMinutes(523));
-
-// -------------------- 4 задача -------------------
-
-function findMaxNumber(numOne, numTwo, numThreе, numFour) {
-
-    if (typeof (numOne) !== 'number' ||
-        typeof (numTwo) !== 'number' ||
-        typeof (numThreе) !== 'number' ||
-        typeof (numFour) !== 'number') {
-        return 0;
-    }
-    return Math.max(numOne, numTwo, numThreе, numFour);
-}
-
-console.log(findMaxNumber(-4, -20000, -123, -442));
-
-// -------------------- 5 задача ------------------- НЕ РЕШЕНА
-
-function numbersFibonacci(num) {
-    if (typeof (num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
-        return '';
-    }
-
-    let result = '';
-    let first = 0;
-    let second = 1;
-
-    for (let i = 0; i < num; i++) {
-        if (i + 1 === num) {
-            result += `${first}`;
-            // Без пробела в конце
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
         } else {
-            result += `${first} `;
+            console.log('error');
+            i--;
         }
-
-        let third = first + second;
-        first = second;
-        second = third;
     }
-
-    return result;
 }
 
-console.log(numbersFibonacci(1));
+// rememberMyFilms();
+
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+
+// detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
